53cca352e7054ea3c30d50d891b06c44f8442416
Ishan Misra <ishan@Ishans-MacBook-Pro-2.local>
2022-09-11 14:49:15 -0400

    Add commands + schema changes

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,9 +1,45 @@
-import { Routes, SlashCommandBuilder } from "discord.js";
+import { Routes, SlashCommandBuilder, SlashCommandMentionableOption, SlashCommandStringOption, SlashCommandSubcommandBuilder } from "discord.js";
 import { REST } from "@discordjs/rest"
 import { config } from "dotenv";
 import path from "path";
 
 const commands = [
+    new SlashCommandBuilder()
+        .setName("stats")
+        .setDescription("Gives you options for viewing statistics in various scenarios.")
+        .addMentionableOption((opt) => {
+            return opt.setName("user").setDescription("Mention a user.").setRequired(false);
+        })
+        .addStringOption((opt) => {
+            return opt.setName("name").setDescription("Write the name of a game.").setRequired(false);
+        })
+        /*
+        .addSubcommandGroup((group) => {
+            return group
+                .setName("options")
+                .setDescription("Options for different sub commands.")
+                .addSubcommand(
+                    (subCommand) => {
+                        return subCommand
+                            .setName("user")
+                            .setDescription("Views the statistics of a specific user.")
+                            .addMentionableOption((opt) => {
+                                return opt.setName("user").setDescription("Mention a user.").setRequired(false);
+                            });
+                    }
+                )
+                .addSubcommand(
+                    (subCommand) => {
+                        return subCommand
+                            .setName("game")
+                            .setDescription("Views the statistics of a specific game based on ID/name")
+                            .addStringOption((opt) => {
+                                return opt.setName("name").setDescription("Write the name of a game.").setRequired(true);
+                            });
+                    }
+                );
+                
+        }),
     new SlashCommandBuilder()
         .setName("activity")
         .setDescription("Shows your game activity since the 1st of the month."),
@@ -17,6 +53,7 @@ const commands = [
             .setName("game")
             .setDescription("The game to show activity for.")
             .setRequired(true)),
+            */
 ]
 
 config({ path: "./.env" });
@@ -24,13 +61,22 @@ const token : string = process.env.TOKEN as string;
 const rest = new REST({ version: "10" }).setToken(token);
 (async() => {
     try {
-        console.log("Started refreshing application (/) commands.");
         await rest.put(
             Routes.applicationCommands(process.env.CLIENT_ID as string),
-            { body: commands },
+            {body: []}
         );
-        console.log("Successfully reloaded application (/) commands.");
-    } catch (error) {
-        console.error(error);
+
+        try {
+            console.log("Started refreshing application (/) commands.");
+            await rest.put(
+                Routes.applicationCommands(process.env.CLIENT_ID as string),
+                { body: commands },
+            );
+            console.log("Successfully reloaded application (/) commands.");
+        } catch (error) {
+            console.error(error);
+        }
+    } catch {
+        throw Error("Could not delete commands.")
     }
 })();
