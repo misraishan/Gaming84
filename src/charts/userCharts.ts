3e0c2b6625a721473fe9a4182d1543cdfd8a536d
HayHay <hayhay@hayhay.dev>
2022-09-12 18:32:42 -0400

    user charts/clean up

---
+++ src/charts/userCharts.ts
@@ -0,0 +1,60 @@
+import { db } from "..";
+import { ChartJSNodeCanvas } from 'chartjs-node-canvas';
+import { randomInt } from "crypto";
+
+let config : any;
+
+function getData(labels : string[], data : string[]) {
+    const randColors = [];
+    for (let i = 0; i < labels.length; i++) {
+        randColors.push(`rgb(${randomInt(256)}, ${randomInt(256)}, ${randomInt(256)})`)
+    }
+    return {
+        labels,
+        datasets: [{
+            label: 'Total game time',
+            data: data,
+            backgroundColor: randColors,
+            hoverOffset: 4
+        }]
+      };
+}
+
+export async function generateDonut(user : string) {
+    const gameList = await db.userGame.findMany({
+        where: {userId: user},
+        include: {game: true}
+    });
+
+    if(gameList[0].userId == undefined) throw Error("Not in db");
+
+    const labels: string[] = [];
+    const data: string[] = []
+
+    gameList.forEach((game) => {
+        labels.push(game.game.name);
+        data.push(game.time);
+    })
+
+    config = {
+        type: 'doughnut',
+        data: getData(labels, data),
+        options: {
+            "devicePixelRatio": 1,
+            "animation": false,
+            "events": [],
+            "responsive": false
+        }
+    };
+
+    const width = 400; //px
+    const height = 400; //px
+    const backgroundColour = "White"; // Uses https://www.w3schools.com/tags/canvas_fillstyle.asp
+    const chart = new ChartJSNodeCanvas({ width, height, backgroundColour});
+
+    const image = await chart.renderToBuffer(config);
+    
+
+    return {image, gameList};
+}
+
--- src/commands/stats/userStats.ts
+++ src/commands/stats/userStats.ts
@@ -1,5 +1,5 @@
-import { ChatInputCommandInteraction, CacheType } from "discord.js";
-import { db } from "../..";
+import { ChatInputCommandInteraction, CacheType, EmbedBuilder, DataResolver, AttachmentBuilder, Attachment, time, TimestampStyles } from "discord.js";
+import { generateDonut } from "../../charts/userCharts";
 import { convertToReadableTime } from "./convertTime";
 
 export async function statsHandler(interaction: ChatInputCommandInteraction<CacheType>) {
@@ -9,25 +9,26 @@ export async function statsHandler(interaction: ChatInputCommandInteraction<Cach
     .replace(/[<@>]/g, "") || interaction.user.id;
 
   if (user) {
-    try {
-      const usr = await db.user.findFirstOrThrow({
-        where: {
-          id: user.toString(),
-        },
-        include: {
-          UserGame: true,
-        },
-      });
+    await generateDonut(user).then(async ({image, gameList}) => {
+      
+      const file = new AttachmentBuilder(image, {name: `image.png`});
 
-      interaction.reply(
-        "game id: " +
-          usr.UserGame[0].gameId +
-          " time spent: " +
-          convertToReadableTime(usr.UserGame[0].time) + "."
-      );
-    } catch (error) {
-      interaction.reply(`Could not find <@${user}> in our database.`);
-    }
+      const embed = new EmbedBuilder()
+        .setColor("Purple")
+        .setTitle(`Hours played as of ${time(Math.floor(Date.now() / 1000), TimestampStyles.ShortDateTime)}`)
+        .setImage(`attachment://image.png`)
+
+      gameList.forEach((val) => {
+        embed.addFields({
+          name: val.game.name,
+          value: convertToReadableTime(val.time)
+        })
+      })
+      
+      await interaction.reply({embeds: [embed], files: [file]})
+    }).catch((err) => {
+      return interaction.reply(`Could not find <@${user}> in our database.`);
+    })
   }
 }
 
