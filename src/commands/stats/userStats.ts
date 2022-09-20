27f2f2dc97e4b64b45ef3835c80aecbacecb5342
Pixwl <pixwl@pixwl.dev>
2022-09-21 01:53:50 +0930

    Formatting

--- src/commands/stats/userStats.ts
+++ src/commands/stats/userStats.ts
@@ -1,35 +1,51 @@
-import { ChatInputCommandInteraction, CacheType, EmbedBuilder, DataResolver, AttachmentBuilder, Attachment, time, TimestampStyles } from "discord.js";
+import {
+  ChatInputCommandInteraction,
+  CacheType,
+  EmbedBuilder,
+  DataResolver,
+  AttachmentBuilder,
+  Attachment,
+  time,
+  TimestampStyles,
+} from "discord.js";
 import { generateDonut } from "../../charts/userCharts";
 import { convertToReadableTime } from "./convertTime";
 
-export async function statsHandler(interaction: ChatInputCommandInteraction<CacheType>) {
-  const user = interaction.options
-    .getMentionable("user")
-    ?.toString()
-    .replace(/[<@>]/g, "") || interaction.user.id;
+export async function statsHandler(
+  interaction: ChatInputCommandInteraction<CacheType>
+) {
+  const user =
+    interaction.options
+      .getMentionable("user")
+      ?.toString()
+      .replace(/[<@>]/g, "") || interaction.user.id;
 
   if (user) {
-    await generateDonut(user).then(async ({image, gameList}) => {
-      
-      const file = new AttachmentBuilder(image, {name: `image.png`});
+    const { image, gameList, error } = await generateDonut(user).catch(async (err) => {
+      await interaction.reply(`Could not find <@${user}> in our database.`);
+      return { image: null, gameList: null, error: err };
+    });
+    if (error != null || image == null) return;
 
-      const embed = new EmbedBuilder()
-        .setColor("Purple")
-        .setTitle(`Hours played as of ${time(Math.floor(Date.now() / 1000), TimestampStyles.ShortDateTime)}`)
-        .setImage(`attachment://image.png`)
+    const file = new AttachmentBuilder(image, { name: `image.png` });
 
-      gameList.forEach((val) => {
-        embed.addFields({
-          name: val.game.name,
-          value: convertToReadableTime(val.time)
-        })
-      })
-      
-      await interaction.reply({embeds: [embed], files: [file]})
-    }).catch((err) => {
-      return interaction.reply(`Could not find <@${user}> in our database.`);
-    })
+    const embed = new EmbedBuilder()
+      .setColor("Purple")
+      .setTitle(
+        `Hours played as of ${time(
+          Math.floor(Date.now() / 1000),
+          TimestampStyles.ShortDateTime
+        )}`
+      )
+      .setImage(`attachment://image.png`);
+
+    gameList.forEach((val) => {
+      embed.addFields({
+        name: val.game.name,
+        value: convertToReadableTime(val.time),
+      });
+    });
+
+    await interaction.reply({ embeds: [embed], files: [file] });
   }
 }
-
-
