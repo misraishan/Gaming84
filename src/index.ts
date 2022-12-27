17da4b23c4d0d1dccc244ba59185f06ef22e47c4
HayHay <dev@hayhay.dev>
2022-12-27 02:37:13 -0500

    Full rework of presence update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 // Discord bot url https://discord.com/api/oauth2/authorize?client_id=1017643869908774963&permissions=139586750528
 // &redirect_uri=http%3A%2F%2Flocalhost%3A3000&scope=bot%20applications.commands
 import { PrismaClient } from "@prisma/client";
-import { Activity, Client, GatewayIntentBits, Presence } from "discord.js";
+import {
+  Activity,
+  ActivityType,
+  Client,
+  GatewayIntentBits,
+  Presence,
+} from "discord.js";
 import { config } from "dotenv";
 import { setTimeout } from "timers";
 import { optIn } from "./commands/optin";
@@ -22,24 +28,30 @@ export const client = new Client({
 export const db = new PrismaClient();
 client.login(token);
 
-const botPresence = () => {return {name: `games on ${client.guilds.cache.size} servers`, type: 1, url: "https://hayhay.dev/"}};
+const botPresence = () => {
+  return {
+    name: `games on ${client.guilds.cache.size} servers`,
+    type: 1,
+    url: "https://hayhay.dev/",
+  };
+};
 
 client.once("ready", () => {
   console.log("Ready!");
   client.user?.setPresence({
-    activities: [botPresence()]
+    activities: [botPresence()],
   });
 });
 
 client.on("guildCreate", () => {
   client.user?.setPresence({
-    activities: [botPresence()]
+    activities: [botPresence()],
   });
 });
 
 client.on("guildDelete", () => {
   client.user?.setPresence({
-    activities: [botPresence()]
+    activities: [botPresence()],
   });
 });
 
@@ -57,133 +69,179 @@ client.on("interactionCreate", async (interaction) => {
     opts.getMentionable("user")
   )
     userGameStats(interaction);
-  else if (command === "stats" && opts.getString("game") && !opts.getString("user"))
+  else if (
+    command === "stats" &&
+    opts.getString("game") &&
+    !opts.getString("user")
+  )
     gameStats(interaction);
   else if (command === "stats") statsHandler(interaction);
 
   // Reset history
-  if (command === "reset") reset(interaction, opts.getString("game"))
+  if (command === "reset") reset(interaction, opts.getString("game"));
 
   // Opt in/opt out manager
   if (command === "optout") optout(interaction);
   if (command === "optin") optIn(interaction);
 });
 
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isAutocomplete()) return;
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isAutocomplete()) return;
 
-	if (interaction.commandName === 'stats' || interaction.commandName === "reset") {
-		const focusedValue = interaction.options.getFocused();
-		const choices = await db.game.findMany({where: {name: {startsWith: focusedValue}}, take: 25});
-		const filtered = choices.filter(choice => choice.name.startsWith(focusedValue));
-		await interaction.respond(
-			filtered.map(choice => ({ name: choice.name, value: choice.name })),
-		);
-	}
+  if (
+    interaction.commandName === "stats" ||
+    interaction.commandName === "reset"
+  ) {
+    const focusedValue = interaction.options.getFocused();
+    const choices = await db.game.findMany({
+      where: { name: { startsWith: focusedValue } },
+      take: 25,
+    });
+    const filtered = choices.filter((choice) =>
+      choice.name.startsWith(focusedValue)
+    );
+    await interaction.respond(
+      filtered.map((choice) => ({ name: choice.name, value: choice.name }))
+    );
+  }
 });
 
-const recentUsers : Map<string, Array<string>> = new Map();
-// Presence listener, can't be in seperate file just bc??? :(
+const recentUsers: Map<
+  string,
+  {
+    name: string;
+    time: number;
+    id: string | null;
+  }
+> = new Map();
+
 client.on("presenceUpdate", async (oldPresence, newPresence) => {
   if (oldPresence?.user?.bot) return;
   const newActivites = newPresence?.activities;
-  const activities = oldPresence?.activities;
+  const userId = newPresence.userId;
 
-  if (!activities) return;
+  const user = await db.user.findFirst({ where: { id: userId } });
 
-  const user = await db.user.findFirst({ where: { id: oldPresence.userId } });
-  if (user != null && !user?.isOptedIn) return;
+  if (!user) await db.user.create({ data: { id: userId } });
 
-  for (const activity of activities) {
-    if (activity.type === 0 && activity.name != "Medal") {
-      if (recentUsers.get(oldPresence.userId)?.filter((name) => name === activity.name)) continue;
-      recentUsers.set(oldPresence.userId, [activity.name].concat(recentUsers.get(oldPresence.userId)??[]))
-      setTimeout(() => {
-        recentUsers.delete(oldPresence.userId);
-      }, 5000)
+  if (!user?.isOptedIn) return;
 
-      let isPresent = false;
-      for (const newActivity of newActivites) {
-        if (newActivity.name === activity.name) {isPresent = true; break;}
-      }
+  let activity = newActivites?.find(
+    (activity) => activity.type === ActivityType.Playing
+  );
 
-      if (isPresent) continue;
-
-      /*console.log(`User ${oldPresence.user?.username} 
-      played ${activity.name} 
-      since ${Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(activity.createdTimestamp)} 
-      till ${Intl.DateTimeFormat('en-US', { dateStyle: 'full', timeStyle: 'long' }).format(Date.now())} 
-      uID ${oldPresence.userId}`)*/
-
-      let game = await db.game.findFirst({ where: { name: activity.name } });
-      if (!game) {
-        game = await db.game.create({
-          data: {
-            name: activity.name,
-          },
-        });
-      }
-
-      let user = await db.user.findFirst({where: {id: oldPresence.userId}})
-      if (!user) {
-        user = await db.user.create({data: {id: oldPresence.userId}})
-      }
-
+  if (!activity) {
+    if (recentUsers.has(userId)) {
       const userGame = await db.userGame.findFirst({
-        where: {
-          userId: oldPresence.userId,
-          AND: {
-            gameId: game?.id,
-          },
-        },
+        where: { userId, game: { name: recentUsers.get(userId)?.name } },
       });
 
-      // if (userGame) console.log(`Old time: ${convertToReadableTime(userGame?.time)}`)
-      const time = getTime(userGame?.time, activity);
-      // console.log(convertToReadableTime(time as string))
+      if (userGame) {
+        await updateUserGame(
+          userGame.id,
+          userGame.time,
+          recentUsers.get(userId)!.time
+        );
+      } else {
+        const game = await db.game.findFirst({
+          where: { name: recentUsers.get(userId)?.name },
+        });
 
-      const newUserGame = await db.userGame.upsert({
-        where: {
-          id: userGame?.id || -1,
-        },
-        create: {
-          gameId: game.id,
-          time: time,
-          userId: user?.id,
-        },
-        update: {
-          time: time,
-        },
-      });
-
-      /*console.log("uID: " + newUserGame?.userId 
-      + " new time: " + convertToReadableTime(newUserGame?.time as string) 
-      + " gameID: " + newUserGame?.gameId + "\n\n")*/
-
-      await db.user.update({
-        where: {
-          id: user.id
-        },
-        data: {
-          lastPlayedGame: game.name,
-          lastPlayedTime: getLastPlayedTime(activity.createdTimestamp),
+        if (game) {
+          createUserGame(userId, game.id, recentUsers.get(userId)!.time);
+        } else {
+          const newGame = await createGame(recentUsers.get(userId)!.name);
+          await createUserGame(
+            userId,
+            newGame.id,
+            recentUsers.get(userId)!.time
+          );
         }
+      }
+    }
+
+    setTimeout(() => {
+      recentUsers.delete(userId);
+    }, 30000);
+    return;
+  }
+
+  const currentUser = recentUsers.get(userId);
+  if (currentUser) {
+    if (
+      activity.applicationId === currentUser.id ||
+      activity.name === currentUser.name
+    ) {
+      return;
+    } else {
+      let game = await db.game.findFirst({ where: { name: currentUser.name } });
+      if (!game) {
+        game = await createGame(currentUser.name);
+      }
+      const userGame = await db.userGame.findFirst({
+        where: { userId: userId, gameId: game?.id },
+      });
+      if (userGame) {
+        await updateUserGame(userGame.id, userGame.time, currentUser.time);
+      } else {
+        await createUserGame(userId, game.id, currentUser.time);
+      }
+
+      recentUsers.set(userId, {
+        name: activity.name,
+        time: Date.now(),
+        id: activity.applicationId,
       });
     }
+  } else {
+    recentUsers.set(userId, {
+      name: activity.name,
+      time: Date.now(),
+      id: activity.applicationId,
+    });
   }
 });
 
-function getLastPlayedTime(startTime : number) {
-  return (Date.now() - startTime).toString()
+async function createGame(name: string) {
+  const game = await db.game.create({ data: { name } });
+  return game;
+}
+async function updateUserGame(
+  userGameId: number,
+  originalTime: string,
+  time: number
+) {
+  const newTime = (parseInt(originalTime) + Date.now() - time).toString();
+
+  const userGame = await db.userGame.update({
+    where: { id: userGameId },
+    data: { time: newTime },
+    include: { user: true, game: true }
+  });
+
+  await db.user.update({
+    where: { id: userGame.user.id },
+    data: { lastPlayedTime: (Date.now() - time).toString(), lastPlayedGame: userGame.game.name },
+  });
 }
 
-function getTime(oldTime: string | undefined | number, activity: Activity) {
-  if (activity.createdTimestamp) {
-    if (oldTime == undefined) {
-      oldTime = 0;
-    } else {
-      oldTime = parseInt(oldTime as string);
-    }
-    return (oldTime + (Date.now() - activity.createdTimestamp)).toString();
-  }
+async function createUserGame(userId: string, gameId: number, time: number) {
+  const newTime = (Date.now() - time).toString();
+
+  const userGame = await db.userGame.create({
+    data: {
+      userId,
+      gameId,
+      time: newTime,
+    },
+    include: { user: true, game: true }
+  });
+
+  userGame.user.lastPlayedGame = userGame.game.name;
+  userGame.user.lastPlayedTime = newTime;
+  await db.user.update({
+    where: { id: userId },
+    data: { lastPlayedGame: userGame.game.name, lastPlayedTime: newTime },
+  });
+  return userGame;
 }
